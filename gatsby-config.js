66b47d5b174334ed3b528fb81f0d8679671837c9
Andrey Solodov <solodov.dev@gmail.com>
2021-01-06 17:28:08 +0300

    Gatsby eslint/prettier setup

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Title from siteMetadata',
+  },
+  plugins: [
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'log',
+        path: `${__dirname}/src/log/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        cwd: process.cwd(), // path to a directory that should be considered as the current working directory
+        watch: true, // format/lint on save
+        initialScan: true, // if true, will format/lint the whole project on Gatsby startup
+        onChangeFullScanLint: false, // if true, on file save always perform full scan lint
+        onChangeFullScanFormat: false, // if true, on file save always perform full scan format
+        prettierLast: false, // if true, will run Prettier after ESLint
+        ignorePatterns: [
+          '**/node_modules/**/*',
+          '**/.git/**/*',
+          '**/dist/**/*',
+          '.cache/**/*',
+          'public/**/*',
+        ], // string or array of paths/files/globs to ignore
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function App() {
-  return <div>My log site</div>;
+export default function App(props: { data: any }): JSX.Element {
+  return (
+    <div>
+      <h1>About {props.data.site.siteMetadata.title}</h1>
+      <div>My log site</div>
+    </div>
+  );
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
