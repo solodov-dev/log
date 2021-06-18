043a859f1c05c92bf8f82e27a023f3064bc189e2
Andrey Solodov <solodov.dev@gmail.com>
2021-06-18 11:52:23 +0300

    Added script for creating log file

---
+++ make_log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ] || [$1 -eq '']; then
+  echo 'Please input a filename!'
+  exit 0
+fi
+
+pushd src/data
+
+cat > $1.md << EOF
+---
+date: "$(date +"%Y-%m-%d")"
+title: 
+tags: []
+---
+EOF
+
+vim src/data/$1.md
